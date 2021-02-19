c2980fe64ce0630b572745087df7ba9ee8b38e39
Alexey-Mancurov <77353857+Alexey-Mancurov@users.noreply.github.com>
2021-02-19 11:56:26 +0300

    Add files via upload

---
+++ valstikez-git/js/main.js
@@ -0,0 +1,85 @@
+$(function () {
+
+  $('.blog__sliger-inner').slick({
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../images/blog-slick-prev.png" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../images/blog-slick-next.png" alt=""></button>',
+  });
+  $('.client__sliger-inner').slick({
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../images/blog-slick-prev.png" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../images/blog-slick-next.png" alt=""></button>',
+  });
+
+  const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      hide: true,
+    },
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + '</span>';
+      },
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+  $('.works__mixitabs-active').on('click', function () {
+    $('.works__mixitabs-btn').slideToggle();
+  });
+
+
+
+});
+
+$(function(){
+  $('.page-works__slider-inner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../images/blog-slick-prev.png" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../images/blog-slick-next.png" alt=""></button>',
+  });
+});
+
+$(function () {
+  $("#menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top
+    }, 500);
+  });
+  $('.up-btn').on('click', function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top
+    }, 500);
+  });
+});
+
+$(function(){
+  $('.popup-link').magnificPopup();
+});
